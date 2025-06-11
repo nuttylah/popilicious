1effeb992039636ac65f2ad3768d59e7c559af33
alyana246 <nuralyanabilah246@gmail.com>
2025-06-12 00:40:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.getElementById("aboutBtn").addEventListener("click", function () {
   window.location.href = "about.html";
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const name = localStorage.getItem("userName");
+  const image = localStorage.getItem("userImage");
+
+  if (name) {
+    document.getElementById("homeUserName").textContent = `Welcome, ${name}!`;
+  }
+
+  if (image) {
+    document.getElementById("homeProfileImage").src = image;
+  }
+});
